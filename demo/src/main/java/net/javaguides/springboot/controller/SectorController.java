06ffbf4a1e148585ae56074cb187b3fb4bdeecec
Gabriel_Kaique <79485516+gabrielkaique2@users.noreply.github.com>
2025-01-30 22:51:16 -0300

    initial commit

---
+++ demo/src/main/java/net/javaguides/springboot/controller/SectorController.java
@@ -0,0 +1,53 @@
+package net.javaguides.springboot.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import net.javaguides.springboot.model.Sector;
+import net.javaguides.springboot.repository.SectorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/sectors")
+public class SectorController {
+
+    @Autowired
+    private SectorRepository sectorRepository;
+
+    @GetMapping
+    public List<Sector> getAllSectors() {
+        return sectorRepository.findAll();
+    }
+
+    @PostMapping
+    public Sector createSector(@RequestBody Sector sector) {
+        return sectorRepository.save(sector);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Sector> getSectorById(@PathVariable Long id) {
+        Sector sector = sectorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Sector not found with id " + id));
+        return ResponseEntity.ok(sector);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSector(@PathVariable Long id) {
+        Sector sector = sectorRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Sector not found with id " + id));
+        sectorRepository.delete(sector);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ demo/src/main/java/net/javaguides/springboot/model/Sector.java
@@ -0,0 +1,55 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "sector")
+public class Sector {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "sector", cascade = CascadeType.ALL)
+    private List<Employee>employees;
+
+    // constructors
+
+    public Sector() {};
+
+    public Sector(String name) {
+        super();
+        this.name = name;
+    }
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee>employees){
+        this.employees = employees;
+    }
+
+}
